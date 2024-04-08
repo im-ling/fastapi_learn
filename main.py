c7bd435719749c764a134d93f9dd210f970c536a
ling <tobeabetterman_dl@163.com>
2024-04-08 14:38:52 +0800

    feat: async

--- main.py
+++ main.py
@@ -6,13 +6,12 @@ app = FastAPI()
 
 
 @app.get("/")
-def read_root():
+async def read_root():
     return {"Hello": "World"}
 
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
+async def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
-
 
 # http://127.0.0.1:8000/items/5?q=somequery
