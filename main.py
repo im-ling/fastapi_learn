5638d599b8aa17bd78a201c2c092aff21ba09bf3
ling <tobeabetterman_dl@163.com>
2024-06-28 19:45:27 +0800

    feat: 2 workers

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 from fastapi import FastAPI
-
+import uvicorn
 app = FastAPI()
+
+# 路径参数无参数
+# http://127.0.0.1:8000/user/me
+@app.get("/user/me")
+async def read_me():
+    return {"me": '1'}
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=7766, workers=2)
